05e4b2fd5f5609d738f8aa8daa791897389f50cf
Harry Nicholls <harry.m.nicholls@gmail.com>
2023-02-07 12:53:08 -0500

    Removes caps handler

--- index.py
+++ index.py
@@ -37,11 +37,6 @@ async def question(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=answer)
 
 
-async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text_caps = ' '.join(context.args).upper()
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
-
-
 async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
 
@@ -52,12 +47,10 @@ if __name__ == '__main__':
 
     question_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), question)
     start_handler = CommandHandler('start', start)
-    caps_handler = CommandHandler('caps', caps)
     unknown_handler = MessageHandler(filters.COMMAND, unknown)
 
     application.add_handler(start_handler)
     application.add_handler(question_handler)
-    application.add_handler(caps_handler)
     application.add_handler(unknown_handler)
 
     application.run_polling()
